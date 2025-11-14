32949c90bee4a7e8d18e60b5904dcbd4c0c3ba6a
Pratap Patra <pratappatra078@gmail.com>
2025-11-14 23:01:29 +0530

    Arranged all files

--- 02_AI Assistance -Jarvis/musicLibray.py
+++
@@ -1,4 +0,0 @@
-music={
-    "kishori":"https://www.youtube.com/watch?v=hjBMTHOj00M&list=RDhjBMTHOj00M&start_radio=1",
-    "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1",
-}
