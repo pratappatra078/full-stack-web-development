7fd7c23971d0d0d7a5ccf5490c3bc9da96ae11aa
Pratap Patra <pratappatra078@gmail.com>
2025-12-31 23:55:30 +0530

    bookmarks project added

---
+++ bookmarks/script.js
@@ -0,0 +1,23 @@
+fetch("links.json")
+    .then(Response => Response.json())
+    .then(links =>{
+        document.querySelectorAll("a[data-key]").forEach(anchor => {
+            const key = anchor.dataset.key;
+            if(links[key]){
+                anchor.href = links[key];
+                anchor.target = "_blank";
+            }
+        });
+    })
+    .catch(error => console.error("Error loading links ",error));
+
+document.querySelectorAll(".profile-item, .bookmarks-item").forEach(icon =>{
+    icon.addEventListener("mouseenter",()=>{
+        icon.style.transform = "scale(1.1)";
+        icon.style.boxshadow = "0 6px 14px rgba(0,0,0,0.3)";
+    })
+    icon.addEventListener("mouseleave",()=>{
+        icon.style.transform = "scale(1)";
+        icon.style.boxShadow = "none";
+    })
+})
